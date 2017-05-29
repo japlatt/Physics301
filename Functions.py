1dfc89db6d85cb491c831d1baf09a74b0d3fd9e3
Warren Morningstar <wmorning@Warrens-MacBook-Air.local>
2017-05-28 23:51:29 -0700

    Added skeleton of DEOptimizer class.  #3

--- Functions.py
+++ Functions.py
@@ -20,3 +20,53 @@ def Fw(t,t0,F0,sigma):
         F = np.average(F0,weights=weights)
     
     return F
+    
+    
+    
+class DEOptimizer(object):
+    """
+    This will be our program that we use to optimize our posterior, prior to
+    sampling.  It utilizes Self_Adaptive Differential Evolution to explore
+    the parameters while avoiding falling into local minima.
+    """
+    def __init__(self,NWalkers):
+        """
+        Initialize the optimizer
+        """
+        self.NWalkers = NWalkers
+    
+    def Initialization(self,priorup,priordn):
+        """
+        Set the initalization region
+        """
+        return
+    def Mutation(self):
+        """
+        Mutate the vectors, using the 4 rules.
+        """
+        return
+    def Recombination(self):
+        """
+        Recombine the donor vectors with the current vectors
+        to create the trial vectors
+        """
+        return
+    def Selection(self,likelihoodfunction):
+        """
+        call likelihoodfunction with the trial vectors, accept
+        if it improves the fit, and replace existing vectors
+        with the trial vectors.
+        """
+        return
+    def UpdateStrategyProbs(self):
+        """
+        Update the probabilities that a given strategy will be attempted, as well
+        as CR and F for recombination.
+        """
+        return
+    def Optimize(Niter,convThresh):
+        """
+        Run the optimization for either Niter iterations, or until the rms scatter 
+        over all the dimensions is lower than convThresh
+        """
+        return
