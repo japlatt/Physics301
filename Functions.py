877b18b5edb9513634726ba720d1063cab3d3e01
Jason <Jason@Jasons-MacBook-Pro.local>
2017-05-29 00:08:35 -0700

    Added comments to Functions.py

--- Functions.py
+++ Functions.py
@@ -1,27 +1,42 @@
 import numpy as np
 import emcee
 import kplr
-import batman
+#import batman
 
 def Fw(t,t0,F0,sigma):
-	"""
-	Compute locally weighted F(t) given samples of t0 and
-    f0, weighted by some timescale sigma
-    
-    returns 0 if no points within ~5 sigma.
-    
     """
-    
+    Compute locally weighted F(t) given samples of t0 and
+    f0, weighted by some timescale sigma
+
+    returns 0 if no points within ~5 sigma.
+
+    """
     weights = np.exp(-0.5*(t-t0)**2/sigma**2)/np.sqrt(2*np.pi*sigma**2)
-    
+
     if np.isclose(np.sum(weights),0):
         F = 0.0
     else:
         F = np.average(F0,weights=weights)
-    
+
     return F
 
-def correlate(flux, n_coors = 80, std_filter = 5):
+def correlate(flux, n_coors = 80,filt = False, std_filter = 5):
+    """
+    Compute auto-correlation function of signal
+
+    @parameters:
+    flux - the signal to be autocorrelated over. Needs
+    to be a continuous function from 0 - t, with masked out points
+    set to 0.
+
+    n_coors - Max number of data points to correlated over.  Will
+    compute correlations from 0 - n_coors
+
+    filt - Boolean to filter or not to filter
+F
+    std_filter - Mask out outliers with signal > std_filter*std
+
+    """
     coor = np.zeros(n_coors)
     mean = np.nanmean(flux)
     standard_dev = np.nanstd(flux)
@@ -29,12 +44,16 @@ def correlate(flux, n_coors = 80, std_filter = 5):
         I1 = copy(flux)
         I2 = array(list(flux[delta:]) + list(flux[0:delta]))
 
-        inds1 = (I1 >= mean - std_filter*standard_dev) & \
-                (I1 <=  mean + std_filter*standard_dev) & \
-                 (I1 >= 0.5)
-        inds2 = (I2 >= mean - std_filter*standard_dev) & \
-                (I2 <=  mean + std_filter*standard_dev) & \
-                (I2 >= 0.5)
+        inds1 = (I1 >= 0.5)
+        inds2 = (I2 >= 0.5)
+
+        if(filt):
+            inds1 = inds1 & (I1 >= mean - std_filter*standard_dev) & \
+                            (I1 <=  mean + std_filter*standard_dev)
+                
+            inds2 = inds2 & (I2 >= mean - std_filter*standard_dev) & \
+                            (I2 <=  mean + std_filter*standard_dev)
+
         inds = inds1 & inds2
         N = sum(inds)
         coor[delta] = sum((I1[inds]*I2[inds]))/N
